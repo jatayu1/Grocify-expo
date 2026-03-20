2601d45eb45faae8c4af66de61078f152646c508
jatayu1 <dakash682@gmail.com>
2026-03-20 14:24:24 +0530

    tailwind setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,11 +4,7 @@ import { Link } from "expo-router";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen. 12345</Text>
-      <Image
-        source={require("../../assets/images/icon.png")} 
-        style={{ width: 100, height: 100, borderRadius: 20, marginTop: 20}}
-        />
+      <Text className="text-red-500 text-4xl bg-purple-500">Edit src/app/index.tsx to edit this screen. 12345</Text>
     </View>
   );
 }
